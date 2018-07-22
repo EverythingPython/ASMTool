10f086b4060693e9c02fcc5c489547ec131875f4
FFengIll <fengyouzheng@gmail.com>
2018-07-22 17:31:19 +0800

    update.

--- check_jcc.py
+++ check_jcc.py
@@ -1,10 +1,17 @@
 # coding=utf-8
+
 import angr
-from angr import SimState
-import pwn
 import pyvex
-import claripy
+from angr import SimState
+
+import unicorn
+from unicorn import *
+from unicorn.x86_const import *
+
+import pwn
+
 import archinfo
+
 import logging.config
 import logging.handlers
 import logging
@@ -20,7 +27,7 @@ config = {
 }
 logging.config.dictConfig(config)
 
-verbose = False
+verbose = 1
 
 # bit offset of each flag
 eflags = {
@@ -38,15 +45,6 @@ regs = [
     'eflags',
 ]
 
-pwn.context.log_level = logging.INFO
-angr.loggers.default_level = logging.INFO
-
-add_options = {
-    angr.options.INITIALIZE_ZERO_REGISTERS,
-    angr.options.CONCRETIZE,
-}
-engine = angr.SimEngineVEX()
-
 # place the jcc pair by pair
 jcc_list = [
     'jz', 'jnz',  # 0, ==
@@ -61,85 +59,214 @@ jcc_list = [
 ]
 
 
-def check_jcc(state, jcc=None):
-    # TODO: this does not work now!
-    """
+class Checker:
+    def __init__(self):
+        self.state = None
 
-    :param state:
-    :param jcc:
-    :return:
-    """
-    feasible = []
-    infeasible = []
-    if jcc:
-        pending = [jcc]
-    else:
-        pending = jcc_list
+    def print_reg(self):
+        raise NotImplementedError
 
-    for jcc in pending:
-        asm_code = 'target:; {} target; '.format(jcc)
-        jmp_code = pwn.asm(asm_code)
-        code_size = len(jmp_code)
+    def print_flag(self):
+        raise NotImplementedError
 
-        addr = state.regs.eip.args[0]
-        irsb = pyvex.IRSB(jmp_code, addr, archinfo.ArchX86(), code_size)
-        # irsb.pp()
+    def print_ctx(self):
+        if verbose:
+            self.print_reg()
+            self.print_flag()
 
-        # we simulate the ins and get the successor state
-        simsucc = engine.process(state, irsb, inline=False)
-        succ = simsucc.successors[0]
+    def check_jcc(self):
+        raise NotImplementedError
 
-        # judge the jcc by the successor state
-        eip = succ.regs.eip.args[0]
-        if eip == addr:
-            feasible.append(jcc)
-        elif eip == addr + code_size:
-            infeasible.append(jcc)
+    def run_bin(self, bin_code):
+        raise NotImplementedError
+
+    def run_asm(self, asm_code):
+        bin_code = self.assemble(asm_code)
+        state = self.run_bin(bin_code)
+        return state
+
+    @staticmethod
+    def assemble(asm_code):
+        return pwn.asm(asm_code)
+
+
+class UC_Checker(Checker):
+    def __init__(self):
+        self.state = None
+        self.engine = Uc(UC_ARCH_X86, UC_MODE_32)
+
+    def check_jcc(self, asm_code, jcc=None):
+        # TODO: this does not work now!
+        """
+
+        :param state:
+        :param jcc:
+        :return:
+        """
+
+        self.state = self.run_asm(asm_code)
+
+        state = self.state
+        engine = self.engine
+
+        self.print_ctx()
+
+        # feasible = []
+        # infeasible = []
+        # if jcc:
+        #     pending = [jcc]
+        # else:
+        #     pending = jcc_list
+        #
+        # for jcc in pending:
+        #     asm_code = 'target:; {} target; '.format(jcc)
+        #     jmp_code = pwn.asm(asm_code)
+        #     code_size = len(jmp_code)
+        #
+        #     addr = state.regs.eip.args[0]
+        #     irsb = pyvex.IRSB(jmp_code, addr, archinfo.ArchX86(), code_size)
+        #     # irsb.pp()
+        #
+        #     # we simulate the ins and get the successor state
+        #     simsucc = engine.process(state, irsb, inline=False)
+        #     succ = simsucc.successors[0]
+        #
+        #     # judge the jcc by the successor state
+        #     eip = succ.regs.eip.args[0]
+        #     if eip == addr:
+        #         feasible.append(jcc)
+        #     elif eip == addr + code_size:
+        #         infeasible.append(jcc)
+        #     else:
+        #         print "impossible eip"
+        #         raise Exception("impossible eip!")
+        #
+        # print "feasible:\n{}".format('\t'.join(feasible))
+        # print "infeasible:\n{}".format('\t'.join(infeasible))
+
+    def run_bin(self, bin_code):
+        engine = self.engine
+
+        addr = 0x100000
+
+        engine.mem_map(addr, 2 * 1024 * 1024)
+        engine.mem_write(addr, bin_code)
+
+        engine.emu_start(addr, addr + len(bin_code))
+
+        eflag = engine.reg_read(UC_X86_REG_EFLAGS)
+        print("EFLAG: {}".format(eflag))
+        return engine
+
+    def print_reg(self):
+        if verbose:
+            for name in regs:
+                regid = getattr(unicorn.x86_const, "UC_X86_REG_{}".format(name.upper()))
+                regval = self.state.reg_read(regid)
+                print "{}:{}".format(name, regval)
+
+    def print_flag(self):
+        if verbose:
+            eflagid = getattr(unicorn.x86_const, "UC_X86_REG_EFLAGS")
+            eflag = self.state.reg_read(eflagid)
+            for name, site in eflags.iteritems():
+                print "{}:{}".format(name, (eflag & 1 << site) >> site)
+
+
+class Angr_Checker(Checker):
+
+    def __init__(self):
+        self.engine = angr.SimEngineVEX()
+
+        self.add_options = {
+            angr.options.INITIALIZE_ZERO_REGISTERS,
+            angr.options.CONCRETIZE,
+        }
+
+    def run_bin(self, bin_code):
+
+        add_options = self.add_options
+        engine = self.engine
+
+        irsb = pyvex.IRSB(bin_code, 0x100000, archinfo.ArchX86(), len(bin_code))
+        if verbose:
+            irsb.pp()
+
+        state = SimState(arch='X86', add_options=add_options, )
+
+        engine.process(state, irsb, inline=True)
+
+        return state
+
+    def print_reg(self):
+        if verbose:
+
+            state = self.state
+
+            for name in regs:
+                regval = state.regs.__getattr__(name)
+                print "{}:{}".format(name, regval)
+
+    def print_flag(self):
+        if verbose:
+            state = self.state
+
+            eflag = state.regs.eflags
+            for name, site in eflags.iteritems():
+                print "{}:{}".format(name, eflag[site])
+
+    def check_jcc(self, asm_code, jcc=None):
+        # TODO: this does not work now!
+        """
+
+        :param state:
+        :param jcc:
+        :return:
+        """
+
+        self.state = self.run_asm(asm_code)
+
+        state = self.state
+        add_options = self.add_options
+        engine = self.engine
+
+        feasible = []
+        infeasible = []
+        if jcc:
+            pending = [jcc]
         else:
-            print "impossible eip"
-            raise Exception("impossible eip!")
+            pending = jcc_list
 
-    print "feasible:\n{}".format('\t'.join(feasible))
-    print "infeasible:\n{}".format('\t'.join(infeasible))
+        for jcc in pending:
+            asm_code = 'target:; {} target; '.format(jcc)
+            jmp_code = pwn.asm(asm_code)
+            code_size = len(jmp_code)
 
+            addr = state.regs.eip.args[0]
+            irsb = pyvex.IRSB(jmp_code, addr, archinfo.ArchX86(), code_size)
+            # irsb.pp()
 
-def print_reg(state):
-    if verbose:
-        for name in regs:
-            print "{}:{}".format(name, state.regs.__getattr__(name))
+            # we simulate the ins and get the successor state
+            simsucc = engine.process(state, irsb, inline=False)
+            succ = simsucc.successors[0]
 
+            # judge the jcc by the successor state
+            eip = succ.regs.eip.args[0]
+            if eip == addr:
+                feasible.append(jcc)
+            elif eip == addr + code_size:
+                infeasible.append(jcc)
+            else:
+                print "impossible eip"
+                raise Exception("impossible eip!")
 
-def print_flag(state):
-    if verbose:
-        for name, site in eflags.iteritems():
-            print "{}:{}".format(name, state.regs.eflags[site])
-
-
-def print_ctx(state):
-    if verbose:
-        print_reg(state)
-        print_flag(state)
-
-
-def run_asm(asm_code):
-    bin_code = pwn.asm(asm_code)
-    state = run_bin(bin_code)
-    return state
-
-
-def run_bin(bin_code):
-    irsb = pyvex.IRSB(bin_code, 0x100000, archinfo.ArchX86(), len(bin_code))
-    if verbose:
-        irsb.pp()
-
-    state = SimState(arch='X86', add_options=add_options)
-
-    engine.process(state, irsb, inline=True)
-
-    return state
+        print "feasible:\n{}".format('\t'.join(feasible))
+        print "infeasible:\n{}".format('\t'.join(infeasible))
 
 
 def excption_0():
+    import claripy
+
     ins_code = "mov eax,-1 ; test eax,eax"
     address = 0x76fcbcfe
 
@@ -185,9 +312,13 @@ if __name__ == '__main__':
     asm = "mov eax,1 ; cmp eax,1;"
     # asm = "mov eax,0x0 ; sub eax,1;"
     print asm
-    state = run_asm(asm)
-    print_ctx(state)
-    check_jcc(state)
-# check_jcc(state, 'ja')
-# check_jcc(state, 'jg')
-# check_jcc(state,)
+
+    ucc = UC_Checker()
+    ucc.check_jcc(asm)
+
+    # ac = Angr_Checker()
+    # ac.check_jcc(asm)
+
+    # check_jcc(state, 'ja')
+    # check_jcc(state, 'jg')
+    # check_jcc(state,)
